95d071da6eee1fbd6ff63ab330d03707dc71214a
BDZNH <pxezhsp@live.com>
2019-01-13 11:45:32 +0800

    重载输出运算符，变得更简单一点

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ extern bool mytime_state;
 int main()
 {
 	mytime mTime;
-	mytime_state=true;
-	std::thread th(show,std::ref(mTime));
+	mytime_state = true;
+	std::thread th(show, std::ref(mTime));
 	th.detach();
 	int year, md, hm;
 	time_t tt;
 	time(&tt);
 	tt = tt + 8 * 3600;
-	tm* t = gmtime(&tt);
+	tm *t = gmtime(&tt);
 	year = t->tm_year + 1900;
 	md = (t->tm_mon + 1) * 100 + t->tm_mday;
 	hm = t->tm_hour * 100 + t->tm_min;
@@ -33,20 +33,14 @@ int main()
 		hm = t->tm_hour * 100 + t->tm_min;
 		if (hm != mTime.gethm())
 		{
-			printf("%d-%02d-%02d %02d:%02d:%02d\n",
-				t->tm_year + 1900,
-				t->tm_mon + 1,
-				t->tm_mday,
-				t->tm_hour,
-				t->tm_min,
-				t->tm_sec);
 			mTime.setyear(year);
 			mTime.setmd(md);
 			mTime.sethm(hm);
+			std::cout << mTime << std::endl;
 		}
 		delay(800);
 	}
-	mytime_state=false;
+	mytime_state = false;
 	delay(1000);
 	return 0;
 }
--- mytime.h
+++ mytime.h
@@ -11,7 +11,7 @@ using namespace std;
 
 class mytime
 {
-public:
+  public:
 	mytime();
 	bool setyear(int);
 	bool setmd(int);
@@ -20,14 +20,19 @@ public:
 	int getyear();
 	int getmd();
 	void setup();
+	friend std::ostream& operator<<(std::ostream &out, const mytime &obj)
+	{
+		out << obj.year << "/" << obj.md / 100 << "/" << obj.md % 10 << "  " << obj.hm / 100 << ":" << obj.hm % 100 << std::flush;
+		return out;
+	}
 	int DIOpin;
 	int SCLKpin;
 	int RCLKpin;
-private:
-	int year;		//年份
-	int md;			//月份和日期
-	int hm;			//小时和分钟
-	
+
+  private:
+	int year; //年份
+	int md;   //月份和日期
+	int hm;   //小时和分钟
 };
 
 void show(mytime &);
