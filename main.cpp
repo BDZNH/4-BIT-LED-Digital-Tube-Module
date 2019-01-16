5d820a58aa3d7823bc463628901f51aca4f2735d
BDZNH <pxezhsp@live.com>
2019-01-16 10:58:28 +0000

    update

--- main.cpp
+++ main.cpp
@@ -13,16 +13,6 @@ int main()
 	th.detach();
 	int year, md, hm;
 	time_t tt;
-	time(&tt);
-	tt = tt + 8 * 3600;
-	tm *t = gmtime(&tt);
-	year = t->tm_year + 1900;
-	md = (t->tm_mon + 1) * 100 + t->tm_mday;
-	hm = t->tm_hour * 100 + t->tm_min;
-	mTime.setyear(year);
-	mTime.setmd(md);
-	mTime.sethm(hm);
-	//mTime.show();
 	while (true)
 	{
 		time(&tt);
